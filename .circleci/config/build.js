c6f4c3797e267649ab50295d3510f4589eaea3eb
Samuel Attard <sattard@salesforce.com>
2022-04-23 01:34:02 -0700

    build: use src-cache-marker to change build jobs

--- .circleci/config/build.js
+++ .circleci/config/build.js
@@ -1,4 +1,5 @@
 const cp = require('child_process');
+const ejs = require('ejs');
 const fs = require('fs-extra');
 const path = require('path');
 const yaml = require('js-yaml');
@@ -6,25 +7,29 @@ const yaml = require('js-yaml');
 const STAGING_DIR = path.resolve(__dirname, '..', 'config-staging');
 
 function copyAndExpand(dir = './') {
-    const absDir = path.resolve(__dirname, dir);
-    const targetDir = path.resolve(STAGING_DIR, dir);
+  const absDir = path.resolve(__dirname, dir);
+  const targetDir = path.resolve(STAGING_DIR, dir);
 
-    if (!fs.existsSync(targetDir)) {
-        fs.mkdirSync(targetDir);
+  if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir);
+  }
+
+  for (const file of fs.readdirSync(absDir)) {
+    if (!file.endsWith('.yml')) {
+      if (fs.statSync(path.resolve(absDir, file)).isDirectory()) {
+        copyAndExpand(path.join(dir, file));
+      }
+      continue;
     }
 
-    for (const file of fs.readdirSync(absDir)) {
-        if (!file.endsWith('.yml')) {
-            if (fs.statSync(path.resolve(absDir, file)).isDirectory()) {
-                copyAndExpand(path.join(dir, file));
-            }
-            continue;
-        }
+    const templatedYaml = ejs.compile(fs.readFileSync(path.resolve(absDir, file), 'utf8'))({
+      srcCacheAlreadyBuilt: !!process.env.ELECTRON_SRC_CACHE_ALREADY_BUILT,
+    });
 
-        fs.writeFileSync(path.resolve(targetDir, file), yaml.dump(yaml.load(fs.readFileSync(path.resolve(absDir, file), 'utf8')), {
-            noRefs: true,
-        }));
-    }
+    fs.writeFileSync(path.resolve(targetDir, file), yaml.dump(yaml.load(templatedYaml), {
+      noRefs: true,
+    }));
+  }
 }
 
 if (fs.pathExists(STAGING_DIR)) fs.removeSync(STAGING_DIR);
